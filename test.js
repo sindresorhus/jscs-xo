13b52dacf8921d254deddab3db597910d72651e1
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-29 16:23:55 +0100

    fix tests

--- test.js
+++ test.js
@@ -2,21 +2,18 @@ import path from 'path';
 import childProcess from 'child_process';
 import test from 'ava';
 
-test(t => {
-	t.plan(3);
-
+test.cb(t => {
 	childProcess.execFile('node', [
-		path.join(__dirname, 'node_modules', '.bin', 'jscs'),
+		path.join('node_modules', '.bin', 'jscs'),
 		'--config=jscsrc',
 		'--reporter=json',
 		'--verbose',
 		'fixture.js'
-	], {
-		cwd: __dirname
-	}, function (_, stdout) {
-		var errors = JSON.parse(stdout)['fixture.js'];
+	], (_, stdout) => {
+		const errors = JSON.parse(stdout)['fixture.js'];
 		t.is(errors.length, 2);
 		t.regexTest(/^validateQuoteMarks/, errors[0].message);
 		t.regexTest(/^requireSemicolons/, errors[1].message);
+		t.end();
 	});
 });
